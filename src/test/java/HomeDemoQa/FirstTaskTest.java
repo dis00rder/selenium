7f6ab6382855dd610e79cd709c3eb256d4191629
dmytro.lehenkiy <disorder.ne@gmail.com>
2023-06-16 18:04:20 +0200

    first + second final

--- src/test/java/HomeDemoQa/FirstTaskTest.java
+++ src/test/java/HomeDemoQa/FirstTaskTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 public class FirstTaskTest {
